293bd33e3adcbc6c7c1d2bb24fdd805f72ec6418
heidi-ann <hh360@cam.ac.uk>
2015-03-26 11:07:10 +0000

    adding process debug function

--- unikernel.ml
+++ unikernel.ml
@@ -11,7 +11,31 @@ module Main (C:CONSOLE) (K:KV_RO) (S:STACKV4) = struct
   module U = S.UDPV4
   module DNS = Dns_server_mirage.Make(K)(S)
 
+  let print_query c query = 
+    let open Packet in
+  	match query.questions with
+  	| [] -> C.log_s c "QCOUNT = 0"
+    | [q] -> C.log_s c (question_to_string q) 
+    | _ -> C.log_s c "QCOUNT > 1"
+
+  let print_answer c query = function 
+  	| None -> C.log_s c "no answer"
+    | Some answer ->
+  		let response = Query.response_of_answer query answer in
+    	C.log_s c (Packet.to_string response)
+
+
+  let debug c process ~src ~dst packet =
+  	print_query c packet
+    >>= fun () -> process ~src ~dst packet
+    >>= (fun ans -> print_answer c packet ans
+    	>>= fun () -> return ans)
+
+
   let start c k s =
     let t = DNS.create s k in
-    DNS.serve_with_zonefile t ~port ~zonefile
+    DNS.eventual_process_of_zonefiles t [zonefile]
+    >>= fun process ->
+    let processor = (processor_of_process (debug c process) :> (module Dns_server.PROCESSOR)) in 
+    DNS.serve_with_processor t ~port ~processor
 end
